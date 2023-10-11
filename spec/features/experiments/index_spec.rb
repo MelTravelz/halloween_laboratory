06e91e85ee8606fd4293500e5e06b312e43dcf4b
Melony Franchini <meltravelz@gmail.com>
2023-10-11 11:58:14 -0600

    Add: test showing order is desc on view page

--- spec/features/experiments/index_spec.rb
+++ spec/features/experiments/index_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe "/experiments", type: :feature do
     @jekyll = Scientist.create!(lab: @electric, name: "Dr. Jekyll or Mr. Hyde", specialty: "sleep deprivation", university: "University of London")
 
     @exp_1 = Experiment.create!(name: "Radio-waves", objective: "understand them", num_months: 6)
-    @exp_2 = Experiment.create!(name: "My hand hurts", objective: "understand why", num_months: 14)
+    @exp_2 = Experiment.create!(name: "My Hand Hurts", objective: "understand why", num_months: 14)
     @exp_3 = Experiment.create!(name: "Monster", objective: "electic-life", num_months: 7)
     @exp_4 = Experiment.create!(name: "Reduced Sleep", objective: "who am I?", num_months: 2)
     @exp_5 = Experiment.create!(name: "More Reduced Sleep", objective: "who am I now?", num_months: 10)
@@ -28,7 +28,7 @@ RSpec.describe "/experiments", type: :feature do
   end
 
   describe "as a user, when I visit the experiment index page" do
-    it "displays the names of all the long-running experiments (6 months +)" do
+    it "displays the names of all long-running experiments (6 months +)" do
       visit "/experiments"
 
       expect(page).to have_content("Long-Running Experiments:")
@@ -36,7 +36,14 @@ RSpec.describe "/experiments", type: :feature do
       expect(page).to have_content("#{@exp_3.name}: #{@exp_3.num_months} months")
       expect(page).to have_content("#{@exp_5.name}: #{@exp_5.num_months} months")
 
-      expect(page).to_not have_content("#{@exp_1.name}")
+      expect(page).to_not have_content(@exp_1.name)
+    end
+
+    it "displays the names of all long-running experiments in DECENDING order" do
+      visit "/experiments"
+
+      expect(@exp_2.name).to appear_before(@exp_5.name)
+      expect(@exp_5.name).to appear_before(@exp_3.name)
     end
   end
 
