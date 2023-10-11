6fd42ba9a32bca5ea86dd87c657c6293a73daeef
Melony Franchini <meltravelz@gmail.com>
2023-10-11 09:43:18 -0600

    Fix: names of experiments for less confusion

--- spec/features/scientists/show_spec.rb
+++ spec/features/scientists/show_spec.rb
@@ -9,21 +9,22 @@ RSpec.describe '/scientists/:id', type: :feature do
     @franky = Scientist.create!(lab: @nuclear, name: "Victor Frankenstein", specialty: "electrical-biology", university: "University of Ingolstadt")
     @jekyll = Scientist.create!(lab: @electric, name: "Dr. Jekyll or Mr. Hyde", specialty: "sleep deprivation", university: "University of London")
 
-    @exp_curie_1 = Experiment.create!(name: "Radio-waves", objective: "understand them", num_months: 6)
-    @exp_curie_2 = Experiment.create!(name: "My hand hurts", objective: "understand why", num_months: 4)
-    @exp_franky_3 = Experiment.create!(name: "Monster", objective: "electic-life", num_months: 5)
-    @exp_jekyll_4 = Experiment.create!(name: "Reduced Sleep", objective: "who am I?", num_months: 2)
-    @exp_jekyll_5 = Experiment.create!(name: "More Reduced Sleep", objective: "who am I now?", num_months: 3)
+    @exp_1 = Experiment.create!(name: "Radio-waves", objective: "understand them", num_months: 6)
+    @exp_2 = Experiment.create!(name: "My hand hurts", objective: "understand why", num_months: 4)
+    @exp_3 = Experiment.create!(name: "Monster", objective: "electic-life", num_months: 5)
+    @exp_4 = Experiment.create!(name: "Reduced Sleep", objective: "who am I?", num_months: 2)
+    @exp_5 = Experiment.create!(name: "More Reduced Sleep", objective: "who am I now?", num_months: 3)
 
-    ScientistExperiment.create!(scientist: @curie, experiment: @exp_curie_1)
-    ScientistExperiment.create!(scientist: @curie, experiment: @exp_curie_2)
+    ScientistExperiment.create!(scientist: @curie, experiment: @exp_1)
+    ScientistExperiment.create!(scientist: @curie, experiment: @exp_2)
 
-    ScientistExperiment.create!(scientist: @franky, experiment: @exp_curie_1)
-    ScientistExperiment.create!(scientist: @franky, experiment: @exp_franky_3)
+    ScientistExperiment.create!(scientist: @franky, experiment: @exp_1)
+    ScientistExperiment.create!(scientist: @franky, experiment: @exp_3)
 
-    ScientistExperiment.create!(scientist: @jekyll, experiment: @exp_curie_1)
-    ScientistExperiment.create!(scientist: @jekyll, experiment: @exp_jekyll_4)
-    ScientistExperiment.create!(scientist: @jekyll, experiment: @exp_jekyll_5)
+    ScientistExperiment.create!(scientist: @jekyll, experiment: @exp_1)
+    ScientistExperiment.create!(scientist: @jekyll, experiment: @exp_3)
+    ScientistExperiment.create!(scientist: @jekyll, experiment: @exp_4)
+    ScientistExperiment.create!(scientist: @jekyll, experiment: @exp_5)
   end
 
   describe "as a user, when I visit a scientist's show page" do
@@ -49,49 +50,49 @@ RSpec.describe '/scientists/:id', type: :feature do
       visit "/scientists/#{@curie.id}"
 
       expect(page).to have_content("Experiments:")
-      expect(page).to have_content("#{@exp_curie_1.name}")
-      expect(page).to have_content("#{@exp_curie_2.name}")
-      expect(page).to_not have_content("Experiments: #{@exp_franky_3.name}")
+      expect(page).to have_content("#{@exp_1.name}")
+      expect(page).to have_content("#{@exp_2.name}")
+      expect(page).to_not have_content("Experiments: #{@exp_3.name}")
     end
 
     it "displays a button to remove each experiement" do
       visit "/scientists/#{@curie.id}"
 
-      within("#experiment-#{@exp_curie_1.id}") do
+      within("#experiment-#{@exp_1.id}") do
         expect(page).to have_button("Remove")
       end
 
-      within("#experiment-#{@exp_curie_2.id}") do
+      within("#experiment-#{@exp_2.id}") do
         expect(page).to have_button("Remove")
       end
     end
 
     it "when I click the Remove button it deletes that experiment and returns to scientist's show page" do
       visit "/scientists/#{@curie.id}"
-      expect(page).to have_content("#{@exp_curie_1.name}")
-      expect(page).to have_content("#{@exp_curie_2.name}")
+      expect(page).to have_content("#{@exp_1.name}")
+      expect(page).to have_content("#{@exp_2.name}")
       
-      within("#experiment-#{@exp_curie_1.id}") do
+      within("#experiment-#{@exp_1.id}") do
         click_button("Remove")
       end
 
       expect(page).to have_current_path("/scientists/#{@curie.id}")
-      expect(page).to have_content("#{@exp_curie_2.name}")
-      expect(page).to_not have_content("#{@exp_curie_1.name}")
+      expect(page).to have_content("#{@exp_2.name}")
+      expect(page).to_not have_content("#{@exp_1.name}")
     end
 
     it "when I click the Remove button it does not impact other scientists working on that experiment" do
       visit "/scientists/#{@curie.id}"
       
-      within("#experiment-#{@exp_curie_1.id}") do
+      within("#experiment-#{@exp_1.id}") do
         click_button("Remove")
       end
 
       visit "/scientists/#{@franky.id}"
-      expect(page).to have_content("#{@exp_curie_1.name}")
+      expect(page).to have_content("#{@exp_1.name}")
 
       visit "/scientists/#{@jekyll.id}"
-      expect(page).to have_content("#{@exp_curie_1.name}")
+      expect(page).to have_content("#{@exp_1.name}")
     end
   end
 end
